fa50ba99aceb7967d08e8cb08691c449826880d8
ANTOINE1128 <makdessyantoine@gmail.com>
2023-10-23 23:27:29 +0300

    update posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,16 @@
 class Post < ApplicationRecord
+  after_save :update_posts_counter
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  def update_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
 
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
 
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 end
