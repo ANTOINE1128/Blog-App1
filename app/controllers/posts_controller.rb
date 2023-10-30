0321ba91de0936d5ac3d69ff6c18a1551f69efa7
ANTOINE1128 <makdessyantoine@gmail.com>
2023-10-30 17:03:48 +0200

    add Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,33 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.new
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :asc)
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
+    @user = User.find(@post.author_id)
+    @comments = @post.comments
+    @current_like = @post.likes.find_by(author_id: current_user.id)
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
