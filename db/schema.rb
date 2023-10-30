210f0437f5aa57f0a98b3a5f5a68ae3b7abcba16
ANTOINE1128 <makdessyantoine@gmail.com>
2023-10-30 20:07:09 +0200

    add forms

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_131638) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "post_id"
+    t.integer "author_id"
+    t.integer "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -34,14 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_131638) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id"
+    t.integer "author_id"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,9 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_131638) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
 end
