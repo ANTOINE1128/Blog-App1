d8665fe5dd47f993caa2cfe3157400c8cef4659c
ANTOINE1128 <makdessyantoine@gmail.com>
2023-10-30 12:58:10 +0200

    add the correct tests for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,51 +1,29 @@
 require 'rails_helper'
 
-describe UsersController, type: :request do
+describe Post, type: :request do
   before do
     @user = User.create(name: 'Sam', photo: 'photo.url', bio: 'Excellent', posts_counter: 0)
+    @post = Post.create(author: @user, title: 'Let us go', text: 'This is the text for the post')
   end
 
-  describe 'GET #index' do
-    it 'responds with success' do
-      get '/users'
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the index template' do
-      get '/users'
-      expect(response).to render_template('users/index')
+  describe 'routes with post_controller' do
+    it 'should render the index action correctly' do
+      get "/users/#{@user.id}/posts"
+      expect(response).to be_successful
+      expect(response).to render_template('posts/index')
       expect(response).to render_template(layout: 'layouts/application')
     end
 
-    it 'assigns @users with all users' do
-      get '/users'
-      expect(assigns(:users)).to eq(User.all)
+    it 'should render the index content correctly' do
+      get "/users/#{@user.id}/posts"
+      expect(response.body).to include('user page')
     end
 
-    it 'includes the correct placeholder text in the response body' do
-      get '/users'
-      expect(response.body).to include('Names of all the users')
-      expect(response.body).to include('Name: Sam')
-      expect(response.body).to include('Posts Counter: 0')
-      expect(response.body).to include('Posts: #&lt;ActiveRecord::Associations::CollectionProxy []&gt;')
-    end
-  end
-
-  describe 'GET #show' do
-    it 'responds with success' do
-      get "/users/#{@user.id}"
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the show template' do
-      get "/users/#{@user.id}"
-      expect(response).to render_template('users/show')
-      expect(response).to render_template(layout: 'layouts/application')
-    end
-
-    it 'includes the correct placeholder text in the response body' do
-      get "/users/#{@user.id}"
-      expect(response.body).to include('this is the detail page for the user with id')
+    it 'should render the show action correctly' do
+      get "/users/#{@user.id}/posts/1"
+      expect(response).to be_successful
+      expect(response).to render_template('posts/show', layout: 'layouts/application')
+      expect(response.body).to include('this is your detail page')
     end
   end
 end
