d1eed2e3eb4c0621268eb501249d342bad1c2e7d
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-10 13:47:59 +0530

    Update the Ability class with user access rules

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,10 @@ class Ability
       can :read, :all
       can :destroy, Post, author_id: user.id
       can :destroy, Comment, author_id: user.id
-
+      can :new, Post, author_id: user.id
+      can :new, Comment, author_id: user.id
+      can :create, Post, author_id: user.id
+      can :create, Comment, author_id: user.id
     end
     #
     # The first argument to `can` is the action you are giving the user
