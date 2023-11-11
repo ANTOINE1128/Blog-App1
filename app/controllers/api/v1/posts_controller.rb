a1c48ab7a501ee01310c1ed8cdfe143861f79c47
ANTOINE1128 <makdessyantoine@gmail.com>
2023-11-11 17:13:34 +0200

    fix linters

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PostsController < ActionController::API
-   skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   def index
     @posts = User.find(params[:user_id]).posts
     render json: @posts
