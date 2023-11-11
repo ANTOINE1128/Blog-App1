427edffff5259558cd2d1c91ab7a65aa876baa53
ANTOINE1128 <makdessyantoine@gmail.com>
2023-11-11 17:10:22 +0200

    delete duplicated  migration file -  update readme file

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PostsController < ActionController::API
+   skip_before_action :verify_authenticity_token
   def index
     @posts = User.find(params[:user_id]).posts
     render json: @posts
